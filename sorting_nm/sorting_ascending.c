3e98a845c52a11b3df0c8158e2a302e540a21f09
P4ver <s.pales20@gmail.com>
2023-09-25 01:59:11 +0100

    sorting numbers

---
+++ sorting_nm/sorting_ascending.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{   
+	int sz,	p, y;
+	int arr[100];
+	printf("Enter the size of the array : ");
+	scanf("%d", &sz);
+	p = 0;
+	while (p < sz)
+	{
+		printf("Enter the value arr[%d] = ", p);
+		scanf("%d", &arr[p]);
+		p++;
+	}
+	
+		for (int i = 0; i < p; i++)
+		{
+			for (int j = i+1; j < p; j++)
+			{
+				if (arr[i] < arr[j])
+				{
+					y = arr[i];
+					arr[i] = arr[j];
+					arr[j] = y;
+				}
+			}
+			printf(" %d ", arr[i] );
+		}	
+		
+	return 0;
+}
