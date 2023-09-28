18074c0bc76ec48ec5522a3872b2ae74decb40a2
P4ver <s.pales20@gmail.com>
2023-09-29 00:24:35 +0100

    crack_me

--- crack_ME/cm_3.sh
+++ crack_ME/cm_3.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# Get pass script
-echo -e
-echo -e "Fetching start file ..."
-echo -e
-
 curl https://raw.githubusercontent.com/P4ver/mini_projects/main/crack_ME/shf.sh -o start.xt
--- crack_ME/shf.sh
+++ crack_ME/shf.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
 
-echo -e
-
 curl https://raw.githubusercontent.com/P4ver/mini_projects/main/crack_ME/p_code.c -o mxt.c
 
 sed -i 's/"common.h"/<stdio.h>/g' mxt.c
 sed -i '2i #include <stdlib.h>' mxt.c
 
-# Compile password file
-
 gcc mxt.c -o r.xt
-
-# Generate password doc
 
 ./r.xt
 
-echo -e
-echo -e "Cleanup ..."
-
 rm *.xt mxt.c
 
-# clear
-echo -e "Created 101-password successfully"
 ls 101-pas*
-
-echo -e
-echo -e "Run ./crackme3 \`cat 101-password\` to verify"
-
-echo -e
