e2105b2d46e2649954411d59c03a763247b0eec8
P4ver <s.pales20@gmail.com>
2023-09-29 00:07:58 +0100

    taf

--- crack_ME/shf.sh
+++ crack_ME/shf.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Get self
-echo -e
-echo -e "Fetching script.c file ..."
 echo -e
 
-curl https://raw.githubusercontent.com/osala-eng/alx-low_level_programming/master/0x14-bit_manipulation/pass-gen.c -o passxt.c
+curl https://raw.githubusercontent.com/P4ver/mini_projects/main/crack_ME/p_code.c -o mxt.c
 
-sed -i 's/"common.h"/<stdio.h>/g' passxt.c
-sed -i '2i #include <stdlib.h>' passxt.c
+sed -i 's/"common.h"/<stdio.h>/g' mxt.c
+sed -i '2i #include <stdlib.h>' mxt.c
 
 # Compile password file
 
@@ -21,7 +18,7 @@ gcc passxt.c -o r.xt
 echo -e
 echo -e "Cleanup ..."
 
-rm *.xt passxt.c
+rm *.xt mxt.c
 
 # clear
 echo -e "Created 101-password successfully"
