30a357034354d83957d4ed071c4bfd51d0a71177
P4ver <s.pales20@gmail.com>
2023-09-29 00:01:21 +0100

    cm3

---
+++ crack_ME/p_code.c
@@ -0,0 +1,17 @@
+#include "common.h"
+int main(void)
+{
+	char *pass = malloc(sizeof(char) * 5);
+	char c[5] = {'H', 'o', 'l', 4, '\0'}, run[100];
+	int i;
+
+	for (i = 0; c[i]; i++)
+		pass[i] = c[i];
+
+	pass[i] = '\0';
+
+	sprintf(run, "echo %s > 101-password", pass);
+	system(run);
+
+	return (0);
+}
