0a8bfe9426bebf294893823c5f865ed01a7db68b
P4ver <s.pales20@gmail.com>
2023-09-29 23:24:11 +0100

    9 is excluded

---
+++ pointer/excld_num.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[5] ={12,9,68,7,9};
+	int *p = arr;
+	int *r = arr;
+
+	for (p = arr; p < arr + 5; p++)
+	{
+		*r = *p;
+		if (*r != 9)
+			r++;
+	}
+	for (p = arr; p < r; p++)
+		printf("%d ", *p);
+	return 0;
+}
+
